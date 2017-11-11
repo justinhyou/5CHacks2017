29e74991c324524bed49220f140b693195ceaaaa
Hyobin You <hyobinyou@gmail.com>
2017-11-11 07:33:10 -0800

    complete for frary example

--- busyness/calculation.py
+++ busyness/calculation.py
@@ -2,34 +2,34 @@ import os
 import numpy as np
 import csv
 import matplotlib.path as mplPath
+from datetime import datetime
 
-#format: quadralateral (x1, y1, x2, y2,...) corresponding to the four corners
-frary_2d = np.array([1, 1, 2, 5, 6, 6, 5, 0])
+frary_2d = np.array([[1, 1], [2, 5], [6, 6], [5, 0]])
 
-#function returns how many people are at frary at a certain time
+#function returns how many people are at frary
 def fraryCount():
-	with open('tues_11_10_2017_5_35.csv', 'rb') as csvfile:
-		has_header = csv.Sniffer().has_header(csvfile.read(1024))
-		reader = csv.reader(csvfile, delimiter=",")
-        print reader
-		if (has_header):
-			next(reader)
-		counter = 0
-		boundary = mplPath.Path(np.array([frary_2d[0]]))
-		for row in reader:
-			if (boundary.contains_point((int(row[0]), int(row[1])))):
-				counter+=1
+    with open("tues_11_10_2017_5_35.csv", "r") as f:
+        reader = csv.reader(f, delimiter=",")
+        counter = 0
+        boundary = mplPath.Path(frary_2d)
+        header = True
+        for line in reader:
+            if (header):
+                header = False
+                continue
+            if (boundary.contains_point((int(line[0]), int(line[1])))):
+                counter+=1
+    return counter
 
-	csvfile.close()
-	return counter
-
+#function inquires the current time and gets closest 5 minute increment
 def timeEstimate():
-    return 'tues_11_10_2017_5_35.csv'
+    return datetime.now()
+#    return 'tues_11_10_2017_5_35.csv'
 
+
+#main function that takes in commandline argument
 def main():
 	return 5
 
-
-
-
-print fraryCount()
+#playzone
+print timeEstimate()
